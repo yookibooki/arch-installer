1643a354644d3193f4df25295689c15c53441399
yookibooki <61198899+yookibooki@users.noreply.github.com>
2026-02-05 09:35:57 +0500

    add modern cli tools and missing packages

--- init.sh
+++ init.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 set -euo pipefail
-(($EUID==0)) && echo "Do not run as root" && exit 1
+(($EUID == 0)) && echo "Do not run as root" && exit 1
 sudo -v
 
 SETUP_DIR="$HOME/.arch-installer"
 
-PACMAN_PKGS=(zed i3-wm dmenu alsa-utils noto-fonts noto-fonts-cjk noto-fonts-emoji noto-fonts-extra ttf-firacode-nerd ttf-nerd-fonts-symbols ttf-nerd-fonts-symbols-common fontconfig libxft git gitui github-cli neovim chezmoi docker docker-compose postgresql jq fzf btop tmux less pass openssh efibootmgr brightnessctl reflector redshift rsync unzip xclip xdotool iwd zram-generator)
+PACMAN_PKGS=(zed i3-wm dmenu alsa-utils noto-fonts noto-fonts-cjk noto-fonts-emoji noto-fonts-extra ttf-firacode-nerd ttf-nerd-fonts-symbols ttf-nerd-fonts-symbols-common fontconfig libxft git gitui github-cli neovim chezmoi docker docker-compose postgresql jq fzf btop tmux less pass openssh efibootmgr brightnessctl reflector redshift rsync unzip xclip xdotool iwd zram-generator bat eza zoxide git-delta dust tealdeer xh fastfetch fd ripgrep wget)
 AUR_PKGS=(mods pass-secret-service-bin brave-bin)
 
 git clone --depth 1 https://github.com/yookibooki/arch-installer.git "$SETUP_DIR" || true
@@ -13,9 +13,9 @@ git clone --depth 1 https://github.com/yookibooki/arch-installer.git "$SETUP_DIR
 sudo pacman -Syu --noconfirm --needed "${PACMAN_PKGS[@]}"
 
 command -v yay >/dev/null || {
-    git clone --depth 1 https://aur.archlinux.org/yay-bin.git
-    (cd yay-bin && makepkg -si --noconfirm)
-    rm -rf yay-bin
+	git clone --depth 1 https://aur.archlinux.org/yay-bin.git
+	(cd yay-bin && makepkg -si --noconfirm)
+	rm -rf yay-bin
 }
 yay -S --noconfirm --needed "${AUR_PKGS[@]}"
 
